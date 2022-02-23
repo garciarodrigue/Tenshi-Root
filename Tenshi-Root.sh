291e1f6682b679468796e8bca2026e47d89da9a7
TENSHI101 <48865498+garciarodrigue@users.noreply.github.com>
2022-02-23 12:50:00 -0600

    Update Tenshi-Root.sh
    Actualización2.1.0

--- Tenshi-Root.sh
+++ Tenshi-Root.sh
@@ -1,11 +1,396 @@
-echo "Instalando Figlet"
-pkg install figlet
-
+echo "instalando Figle"t
+pkg install figlet -y
 figlet "TENSHI"
 
-echo "Instalando Root"
+echo "instalando proot"
 pkg install proot
 
 echo "EJECUTANDO ROOT"
-echo "ya eres Root"
-proot -0 -w ~ $PREFIX/bin/bash 
+
+echo -e '\e[0m'
+clear
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo""
+echo""
+echo""
+echo""
+echo -e '               /█1%...................../'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "  "
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '                /█3%....................../'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '                /█6%......................./'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '                /██10%....................../'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '                /██12%....................../'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '                /███15%..................../'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '               /█████19%..................../'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '               /██████26%..................../'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '                /████████30%................../'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '                /██████████45%................/'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '                /█████████████50%............./'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '                /████████████████60%........../'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '                /███████████████████88%......./'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '                /██████████████████████95%..../'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '\e[38;5;56m'
+echo '
+    ░▀▀█▀▀░
+    ░░▒█░
+    ░░▒█░░
+
+
+'
+echo -e '      \e[0m         [██████████████████████ 100%]'
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+sleep 1.8
+clear
+echo ''
+
+proot -0 -w ~ $PREFIX/bin/bash
+
+echo "Saliste De Usuario Root"
